97592d1e5991e65bd854dde51cc7dffc5c858bc8
ab8454 <ab8454@naver.com>
2021-08-13 00:42:03 +0900

    feat: add print util

---
+++ minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunwkim <hyunwkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/12 23:31:16 by hyunwkim          #+#    #+#             */
+/*   Updated: 2021/08/12 23:35:56 by hyunwkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+
+void ft_putnbr(int n);
+void ft_putchar(char c);
+void ft_putstr(char *s);
+int ft_atoi(char *s);
+int ft_strlen(char *s);
+
+#endif
---
+++ print_util.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_util.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyunwkim <hyunwkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/12 23:33:05 by hyunwkim          #+#    #+#             */
+/*   Updated: 2021/08/12 23:33:08 by hyunwkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int ft_strlen(char *s)
+{
+    int len;
+
+	len = 0;
+    while (s[len])
+        len++;
+	return (len);
+}
+
+int ft_atoi(char *s)
+{
+	unsigned int num;
+	unsigned int idx;
+
+	num = 0;
+	while ('0' <= *s && *s <= '9')
+	{
+		num = num * 10 + *s - '0';
+		s++;
+	}
+	return (num);
+}
+void ft_putstr(char *s)
+{
+	write(1, s, ft_strlen(s));
+}
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int n)
+{
+	if (n >= 10)
+    {
+		ft_putnbr(n / 10);
+		ft_putchar(n % 10 + '0');
+    }
+	if (n < 10)
+		ft_putchar(n + '0');
+}
